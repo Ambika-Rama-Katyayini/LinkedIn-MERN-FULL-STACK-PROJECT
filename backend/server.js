4fccf70fae15a2a543e4c6ea0d21f023ab2b2840
Ambika-Rama-Katyayini <ambikaramakatyayini.k@gmail.com>
2025-03-03 01:24:09 +0530

    signup feature implemented

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 
-import authRoutes from "./routes/auth.routes.js";
+import authRoutes from "./routes/auth.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -10,10 +10,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // to parse JSON request bodies
+
 // authentication - version our api(/api/v1/auth) to save our old app customers along with new customers.
 app.use("/api/v1/auth", authRoutes);
 // app.use("/api/v2/auth")
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  connectDB();
 });
