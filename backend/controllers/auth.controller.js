5e021a67ef72a9c3efc747a7765a4bf19cd4ba57
Ambika-Rama-Katyayini <ambikaramakatyayini.k@gmail.com>
2025-03-03 23:58:40 +0530

    Testing done to Create a New User using Postman

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,10 +1,14 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-export const signup = (req, res) => {
+export const signup = async (req, res) => {
     // res.send("signup");
     try {
         const {name, username, email, password} = req.body;
+
+        if(!name || !username || !email || !password) {
+            return res.status(400).json({message: "All fields are required"});
+        }
         const existingEmail = await User.findOne({email});
         if (existingEmail) {
             return res.status(400).json({message: "Email already exists"});
@@ -34,7 +38,7 @@ export const signup = (req, res) => {
         
         const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: "3d"});
         res.cookie("jwt-linkedin", token, {
-            httpOnly: true, // prevent XSS attack
+            httpOnly: true, // prevent XSS attack (Input Validation & Sanitization: in Express.js using validator package| use "helmet" middleware)
             maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days
             sameSite: "strict", // prevent CSRF attack (Cross-Site Request Forgery)
             secure: process.env.NODE_ENV === "production", // prevents men-in-the-middle attacks
